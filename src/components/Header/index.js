45aba962e362313c44d434cc07cfb0ee388775d5
Jesse B. Hannah <jesse@jbhannah.net>
2018-03-08 15:59:48 -0700

    Style layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,24 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
-const Header = ({ title }) => (
-  <div>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-        >
+const Header = ({ title, className }) => (
+  <header className={className}>
+    <div>
+      <h1>
+        <Link to="/">
           {title}
         </Link>
       </h1>
     </div>
-  </div>
+  </header>
 )
 
 Header.propTypes = {
