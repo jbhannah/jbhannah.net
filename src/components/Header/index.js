32b24931618ffa317d6bae62b005e853f35e2e86
Jesse B. Hannah <jesse@jbhannah.net>
2018-03-08 22:04:00 -0700

    Header icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
+import github from '../../images/github.svg'
+import linkedin from '../../images/linkedin.svg'
+import twitter from '../../images/twitter.svg'
+
 const Header = ({ title, className }) => (
   <header className={className}>
-    <div>
-      <h1>
-        <Link to="/">
-          {title}
-        </Link>
-      </h1>
-    </div>
+    <h1>
+      <Link to="/">
+        {title}
+      </Link>
+    </h1>
+    <ul>
+      <li>
+        <a href="https://github.com/jbhannah" target="_blank">
+          <img alt="jbhannah on GitHub" src={github} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/jbhannah" target="_blank">
+          <img alt="jbhannah on LinkedIn" src={linkedin} />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/jbhannah" target="_blank">
+          <img alt="@jbhannah on Twitter" src={twitter} />
+        </a>
+      </li>
+    </ul>
   </header>
 )
 
