d702eaab87612e845d4dd5a980b72661fcf1538e
Jesse B. Hannah <jesse@jbhannah.net>
2019-01-21 12:01:34 -0700

    Remove deprecated gatsby-link references

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
-import React from "react"
+import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import Link from "gatsby-link"
+import React from "react"
 
 const year = new Date().getFullYear()
 
