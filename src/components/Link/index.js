4adfb1a39013feb66bf6d1d8fca75caac7241c37
Jesse B. Hannah <jesse@jbhannah.net>
2019-01-25 21:25:56 -0700

    Add rel="noopener" target="_blank" to external links

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -8,12 +8,17 @@ const styles = {
   },
 }
 
-const Link = props => {
-  if (props.hasOwnProperty("to")) {
-    return <GatsbyLink css={styles} {...props} />
+const Link = ({ to, href, ...props }) => {
+  if (to) {
+    return <GatsbyLink css={styles} to={to} {...props} />
   }
 
-  return <a css={styles} {...props} />
+  if (/^https?:\/\/(?!jbhannah\.net)/.test(href)) {
+    props.rel = "noopener"
+    props.target = "_blank"
+  }
+
+  return <a css={styles} href={href} {...props} />
 }
 
 export default Link
