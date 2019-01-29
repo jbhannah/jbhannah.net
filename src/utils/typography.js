b98112a05fc1fa1e47dec237acacee9b1615ecda
Jesse B. Hannah <jesse@jbhannah.net>
2019-01-29 12:55:50 -0700

    Fix base font sizes for media queries

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,3 +1,4 @@
+import verticalRhythm from "compass-vertical-rhythm"
 import "hack-font/build/web/fonts/hack-regular-subset.woff"
 import "hack-font/build/web/fonts/hack-regular-subset.woff2"
 import "hack-font/build/web/hack-subset.css"
@@ -16,37 +17,34 @@ import "typeface-varela-round/files/varela-round-latin-400.woff2"
 import Typography from "typography"
 import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants"
 import lincolnTheme from "typography-theme-lincoln"
-import { linkColor, mq } from "./styles"
+import { mq } from "./styles"
 
 lincolnTheme.baseFontSize = "16px"
 lincolnTheme.googleFonts = []
-lincolnTheme.overrideThemeStyles = ({ rhythm }) => ({
-  blockquote: {
-    borderLeft: `${rhythm(3 / 16)} solid ${linkColor}`,
-    marginLeft: rhythm(-3 / 4),
-    paddingLeft: rhythm(9 / 16),
-  },
-  "code[class*=language-], pre[class*=language-]": {
-    fontFamily: "Hack, monospace",
-    fontFeatureSettings: '"liga" 0',
-    MozFontFeatureSettings: '"liga" 0',
-    WebkitFontFeatureSettings: '"liga" 0',
-  },
-  [MOBILE_MEDIA_QUERY]: {
-    blockquote: {
-      borderLeft: `${rhythm(3 / 16)} solid ${linkColor}`,
-      marginLeft: rhythm(-3 / 4),
-      paddingLeft: rhythm(9 / 16),
+lincolnTheme.overrideThemeStyles = () => {
+  const vr = verticalRhythm({
+    baseFontSize: "17px",
+    baseLineHeight: "28px",
+  })
+  const html = { ...vr.establishBaseline() }
+  return {
+    html,
+    "code[class*=language-], pre[class*=language-]": {
+      fontFamily: "Hack, monospace",
+      fontFeatureSettings: '"liga" 0',
+      MozFontFeatureSettings: '"liga" 0',
+      WebkitFontFeatureSettings: '"liga" 0',
     },
-  },
-  [mq.sm]: {
-    blockquote: {
-      borderLeft: `${rhythm(6 / 16)} solid ${linkColor}`,
-      marginLeft: 0,
-      paddingLeft: rhythm(10 / 16),
+    [MOBILE_MEDIA_QUERY]: {
+      html,
     },
-  },
-})
+    [mq.sm]: {
+      html: {
+        fontSize: "100%",
+      },
+    },
+  }
+}
 
 const typography = new Typography(lincolnTheme)
 const { rhythm, scale } = typography
