15af9e0a9fdaebef4c7cfdadc22cf3a5ac0ed258
Jesse B. Hannah <jesse@jbhannah.net>
2019-02-06 11:39:01 -0700

    Footer unit test

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,19 +4,18 @@ import Link from "../Link"
 
 const year = new Date().getFullYear()
 
-const Footer = ({ title, className }) => (
-  <footer className={className}>
+const Footer = ({ title }) => (
+  <footer>
     <div>
       Copyright © {year} <Link href="/">{title}</Link>.{" "}
       <Link
         href="https://creativecommons.org/licenses/by-sa/4.0/"
-        target="_blank"
         rel="license"
       >
         Some Rights Reserved
       </Link>
       .{" "}
-      <Link href="https://github.com/jbhannah/jbhannah.net" target="_blank">
+      <Link href="https://github.com/jbhannah/jbhannah.net">
         Source on GitHub
       </Link>
       .
