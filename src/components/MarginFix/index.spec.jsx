0e9ac7d1faa56fc2314da52fe41467aed8873314
Jesse B. Hannah <jesse@jbhannah.net>
2019-02-07 11:08:54 -0700

    MarginFix unit test

---
+++ src/components/MarginFix/index.spec.jsx
@@ -0,0 +1,12 @@
+import { shallow } from "enzyme"
+import React from "react"
+import MarginFix from "."
+
+const TestMarginFix = props => shallow(<MarginFix {...props} />)
+
+describe("MarginFix", () => {
+  it("renders correctly", () => {
+    const tree = TestMarginFix()
+    expect(tree).toMatchSnapshot()
+  })
+})
