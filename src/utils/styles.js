574a8d7348cb3af9fe0b0002d95ef16efed905f2
Jesse B. Hannah <jesse@jbhannah.net>
2019-05-14 12:22:59 -0700

    Fix manifest and _headers generation

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,3 +1,14 @@
+import "hack-font/build/web/fonts/hack-regular-subset.woff2"
+import "hack-font/build/web/hack-subset.css"
+import "prismjs/themes/prism-solarizedlight.css"
+import "typeface-lora"
+import "typeface-lora/files/lora-latin-400.woff2"
+import "typeface-lora/files/lora-latin-400italic.woff2"
+import "typeface-lora/files/lora-latin-700.woff2"
+import "typeface-lora/files/lora-latin-700italic.woff2"
+import "typeface-varela-round"
+import "typeface-varela-round/files/varela-round-latin-400.woff2"
+
 export const breakpoints = { sm: 28, md: 38, lg: 59, xl: 70 }
 export const mq = Object.assign(
   ...Object.entries(breakpoints).map(([k, v]) => ({
