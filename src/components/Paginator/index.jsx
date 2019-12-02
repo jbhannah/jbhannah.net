af846c045b74a55678cead8aa67a51c7c3dbb217
Jesse B. Hannah <jesse@jbhannah.net>
2019-12-02 11:35:46 -0700

    Simplify main paginator component

--- src/components/Paginator/index.jsx
+++ src/components/Paginator/index.jsx
@@ -15,6 +15,53 @@ const paginatorCSS = {
   },
 }
 
+const backLinks = (base, pageBase, currentPage) =>
+  currentPage > 1 ? (
+    <Fragment>
+      <li>
+        <Link href={base}>«</Link>
+      </li>
+      <li>
+        <Link href={`${pageBase}/${currentPage - 1}`}>‹</Link>
+      </li>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <li>«</li>
+      <li>‹</li>
+    </Fragment>
+  )
+
+const forwardLinks = (pageBase, currentPage, numPages) =>
+  currentPage < numPages ? (
+    <Fragment>
+      <li>
+        <Link href={`${pageBase}/${currentPage + 1}`}>›</Link>
+      </li>
+      <li>
+        <Link href={`${pageBase}/${numPages}`}>»</Link>
+      </li>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <li>›</li>
+      <li>»</li>
+    </Fragment>
+  )
+
+const pageLink = (base, pageBase, currentPage) => (_, i) => {
+  const page = i + 1
+  return (
+    <li key={`paginate-${i}`}>
+      {currentPage === page ? (
+        page
+      ) : (
+        <Link href={i === 0 ? base : `${pageBase}/${page}`}>{page}</Link>
+      )}
+    </li>
+  )
+}
+
 const Paginator = ({ base, page: currentPage, numPages }) => {
   const pageBase = base === "/" ? "/page" : `${base}/page`
 
@@ -22,50 +69,11 @@ const Paginator = ({ base, page: currentPage, numPages }) => {
     numPages > 1 && (
       <footer>
         <ul css={paginatorCSS}>
-          {currentPage > 1 ? (
-            <Fragment>
-              <li>
-                <Link href={base}>«</Link>
-              </li>
-              <li>
-                <Link href={`${pageBase}/${currentPage - 1}`}>‹</Link>
-              </li>
-            </Fragment>
-          ) : (
-            <Fragment>
-              <li>«</li>
-              <li>‹</li>
-            </Fragment>
+          {backLinks(base, pageBase, currentPage)}
+          {Array.from({ length: numPages }).map(
+            pageLink(base, pageBase, currentPage)
           )}
-          {Array.from({ length: numPages }).map((_, i) => {
-            const page = i + 1
-            return (
-              <li key={`paginate-${i}`}>
-                {currentPage === page ? (
-                  page
-                ) : (
-                  <Link href={i === 0 ? base : `${pageBase}/${page}`}>
-                    {page}
-                  </Link>
-                )}
-              </li>
-            )
-          })}
-          {currentPage < numPages ? (
-            <Fragment>
-              <li>
-                <Link href={`${pageBase}/${currentPage + 1}`}>›</Link>
-              </li>
-              <li>
-                <Link href={`${pageBase}/${numPages}`}>»</Link>
-              </li>
-            </Fragment>
-          ) : (
-            <Fragment>
-              <li>›</li>
-              <li>»</li>
-            </Fragment>
-          )}
+          {forwardLinks(pageBase, currentPage, numPages)}
         </ul>
       </footer>
     )
