c3b66fa76efaf68000e89c9491e89a3fcdbc160e
Jesse B. Hannah <jesse@jbhannah.net>
2020-06-02 22:11:16 -0700

    Fix importing react-helmet

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,7 @@
 import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Helmet from "react-helmet"
+import { Helmet } from "react-helmet"
 import {
   contentWidth,
   contentWidthColumn,
