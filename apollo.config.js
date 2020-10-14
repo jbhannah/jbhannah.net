1e3d72f48413bf6410dc38e43ac6db0e8572aa1f
Jesse B. Hannah <jesse@jbhannah.net>
2020-10-14 16:47:27 -0700

    Update dependencies

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     addTypename: false,
     excludes: ["./node_modules/gatsby-transformer-sharp/src/*.js"],
-    includes: ["./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js","./node_modules/gatsby-source-contentful/src/*.js","./node_modules/gatsby-source-datocms/fragments/*.js","./node_modules/gatsby-transformer-sharp/src/*.js","./node_modules/gatsby-image/src/*.js","./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js"],
+    includes: ["./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js","./node_modules/gatsby-source-contentful/src/fragments.js","./node_modules/gatsby-source-datocms/fragments/*.js","./node_modules/gatsby-source-sanity/fragments/*.js","./node_modules/gatsby-transformer-sharp/src/fragments.js","./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js"],
     service: {
       name: "gatsbySchema",
       localSchemaFile: "./schema.json"
